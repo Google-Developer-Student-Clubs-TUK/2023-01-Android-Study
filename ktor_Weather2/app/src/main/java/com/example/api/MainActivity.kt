9e68d6b81617e6395b5d3728435bf09d9e72ade4
DoDi <4717jun57@naver.com>
2023-05-22 15:00:05 +0900

    2023/05/22 Update

--- ktor_Weather2/app/src/main/java/com/example/api/MainActivity.kt
+++ ktor_Weather2/app/src/main/java/com/example/api/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.api
 
+import android.net.http.HttpResponseCache.install
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,32 +13,52 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.api.ui.theme.ApiTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 // buld Weather app with Jetpack Compose and ktor
 // using api key https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var weatherTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ApiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        weatherTextView = findViewById(R.id.weatherTextView)
+
+        // API 호출
+        getWeatherData("Seoul")
+    }
+
+    private fun getWeatherData(city: String) {
+        val apiKey = "142040a9bbf97e6ee6ed7ab6540fb6a4"
+        val url = "http://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey"
+
+        val httpClient = HttpClient(Android) {
+            install(JsonFeature)
+        }
+
+        // 비동기로 API 호출
+        lifecycleScope.launch {
+            val response = httpClient.get<WeatherResponse>(url)
+            showWeatherData(response)
+        }
+    }
+
+    private suspend fun showWeatherData(weatherResponse: WeatherResponse) {
+        withContext(Dispatchers.Main) {
+            val temperature = weatherResponse.main.temp
+            val description = weatherResponse.weather[0].description
+
+            val weatherText = "Temperature: $temperature\nDescription: $description"
+            weatherTextView.text = weatherText
         }
     }
 }
-// Composable function
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.fillMaxSize())
-}
-// Preview function
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ApiTheme {
-        Greeting("Android")
-    }
-}
+
+data class WeatherResponse(val main: MainData, val weather: List<WeatherData>)
+data class MainData(val temp: Double)
+data class WeatherData(val description: String)
